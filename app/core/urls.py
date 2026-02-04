36e13167037f7617467dfc4d6ef7c392c9332fad
Darwin Dela Cruz <darwindelacruz0012@gmail.com>
2026-02-04 16:11:08 +0800

    Add root endpoint to confirm API is running

--- app/core/urls.py
+++ app/core/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import home
 
 urlpatterns = [
+    path("", home),  # 👈 root URL
     path("admin/", admin.site.urls),
     path("api/users/", include("users.urls")),
 ]
